7cce57d59720e17fe13eb0723bb29391e2c6488f
Bohdan N <bohdan@Bohdans-Mac-mini.local>
2020-01-30 03:09:53 +0200

    Router + Nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,38 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import ProductsProvider from "../context/ProductsProvider";
+import { Row } from "antd";
+import CartProvider from "../context/CartProvider";
 import Header from "../ui/Header";
 import HomePage from "../pages/HomePage";
 import ProductsPage from "../pages/ProductsPage";
+import SingleProductPage from "../pages/SingleProductPage";
+import CartPage from "../pages/CartPage";
 
 function App() {
   return (
-    <ProductsProvider>
+    <CartProvider>
       <Router>
         <Header />
-
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route exact path="/products">
-            <ProductsPage />
-          </Route>
-        </Switch>
+        <div style={{ padding: "30px" }}>
+          <Row gutter={[16, 16]}>
+            <Switch>
+              <Route exact path="/">
+                <HomePage />
+              </Route>
+              <Route exact path="/products">
+                <ProductsPage />
+              </Route>
+              <Route exact path="/products/:id">
+                <SingleProductPage />
+              </Route>
+              <Route exact path="/cart">
+                <CartPage />
+              </Route>
+            </Switch>
+          </Row>
+        </div>
       </Router>
-    </ProductsProvider>
+    </CartProvider>
   );
 }
 
